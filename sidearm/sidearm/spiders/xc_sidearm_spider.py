f1465f1ff20fcccbceb4e8567bf68ffaf2cb1774
David Cashin <dave.cashin@ca.ibm.com>
2019-08-22 18:02:27 -0400

    Working scrape state for some ontario schools

--- sidearm/sidearm/spiders/xc_sidearm_spider.py
+++ sidearm/sidearm/spiders/xc_sidearm_spider.py
@@ -4,11 +4,28 @@ import scrapy
 
 class XcSidearmSpider(scrapy.Spider):
     name = 'xc_sidearm'
-    allowed_domains = ['queens']
-    start_urls = ['https://gogaelsgo.com/schedule.aspx?path=cross']
+    # TODO: some sites have old sidearm versions, don't scrape well
+    start_urls = ['https://gogaelsgo.com/schedule.aspx?path=cross',
+    'https://varsityblues.ca/schedule.aspx?path=cross',
+    'https://athletics.uwaterloo.ca/schedule.aspx?path=mcross',
+    'https://westernmustangs.ca/schedule.aspx?path=mcross',
+    'https://golancers.ca/schedule.aspx?path=cross',
+    'https://yorkulions.ca/schedule.aspx?path=xcountry',
+    'https://gobadgers.ca/schedule.aspx?path=mcross',
+    # 'http://www.gryphons.ca/schedule.aspx?path=cross'
+    'https://marauders.ca/schedule.aspx?path=mcross',
+    # 'https://equipes.geegees.ca/sports/xc/2019-20/schedule'
+    ]
 
     def parse(self, response):
         for race in response.css('li.sidearm-schedule-game'):
+            # clean up text-only race descriptions for non hyperlinked races
+            descriptor_href = race.css('.sidearm-schedule-game-opponent-name>a::text').get()
+            descriptor = descriptor_href if descriptor_href else ' '.join(race.css('.sidearm-schedule-game-opponent-name::text').get().split())
+
             yield {
-                'date': race.css('.sidearm-schedule-game-opponent-date>span::text').get()
+                'date': race.css('.sidearm-schedule-game-opponent-date>span::text').get(),
+                'location': race.css('.sidearm-schedule-game-location>span::text').get(),
+                'descriptor': descriptor,
+                'travellingSchool': response.url
             }
